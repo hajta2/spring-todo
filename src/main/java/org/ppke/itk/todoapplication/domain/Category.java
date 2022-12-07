ee1a6826135c86b8bb513f6f30ba4d81c5ad6b96
hajta2 <topitomi22@gmail.com>
2022-12-07 22:19:04 +0100

    created DTOs

--- src/main/java/org/ppke/itk/todoapplication/domain/Category.java
+++ src/main/java/org/ppke/itk/todoapplication/domain/Category.java
@@ -7,7 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,6 +29,11 @@ public class Category implements Serializable {
     private Long id;
     private String name;
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    @JsonIgnore
+    private User user;
 
     @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
     private List<Todo> todoList;
--- src/main/java/org/ppke/itk/todoapplication/repositories/CategoryRepository.java
+++ src/main/java/org/ppke/itk/todoapplication/repositories/CategoryRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.ppke.itk.todoapplication.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findAllByUserId(Long userId);
 
     @Query("select t.category from Todo t where t.id = :todoId")
     Category findCategoryByTodoId(Long todoId);
+
 }
