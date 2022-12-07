1884ae6d1100aae0a7cbb974edd6d30ac3020f03
hajta2 <topitomi22@gmail.com>
2022-12-07 21:54:35 +0100

    last minute restart

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO todo_priority(id, name) VALUES (1, 'MINOR');
-INSERT INTO todo_priority(id, name) VALUES (2, 'MAJOR');
-INSERT INTO todo_priority(id, name) VALUES (3, 'CRITICAL');
-
-INSERT INTO todo_status(id, is_done) VALUES (1, true);
-INSERT INTO todo_status(id, is_done) VALUES (2, false);
