8ed8910fe013608a984e621be332d1ebf05f2f7a
hajta2 <topitomi22@gmail.com>
2022-12-07 22:19:14 +0100

    DTOs

--- src/main/java/org/ppke/itk/todoapplication/controllers/dto/TodoDto.java
+++ src/main/java/org/ppke/itk/todoapplication/controllers/dto/TodoDto.java
@@ -1,5 +1,53 @@
 package org.ppke.itk.todoapplication.controllers.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+import org.ppke.itk.todoapplication.domain.Todo;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TodoDto {
 
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Date startDate;
+
+    private boolean done;
+
+    private boolean favorite;
+
+    private CategoryDto category;
+
+    public static Todo toEntity(TodoDto todoDto) {
+        final Todo todo = new Todo();
+        todo.setId(todoDto.getId());
+        todo.setTitle(todoDto.getTitle());
+        todo.setDescription(todoDto.getDescription());
+        todo.setDone(todoDto.isDone());
+        todo.setStartDate(todoDto.getStartDate());
+        todo.setCategory(CategoryDto.toEntity(todoDto.getCategory()));
+
+        return todo;
+    }
+
+    public static TodoDto fromEntity(Todo todo) {
+        return TodoDto.builder()
+                .id(todo.getId())
+                .title(todo.getTitle())
+                .description(todo.getDescription())
+                .startDate(todo.getStartDate())
+                .done(todo.isDone())
+                .build();
+    }
 }
